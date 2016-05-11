b3f5dbeb02721e6144b1e1ba6ae5674b8e9c1e58
KristinaShevtsova <shevtsovakristina1995@rambler.ru>
2016-05-11 22:13:07 +0300

    Рабочий RecyclerView, DichotomousCard

--- app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/QuestionnaireActivity.java
+++ app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/QuestionnaireActivity.java
@@ -1,10 +1,15 @@
 package com.petrsu.cardiacare.smartcarequestionnaire;
 
 import android.os.Bundle;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.sql.Types;
+import java.util.LinkedList;
 
 public class QuestionnaireActivity extends AppCompatActivity {
 
@@ -26,7 +31,42 @@ public class QuestionnaireActivity extends AppCompatActivity {
         QuestionnaireLayoutManager= new LinearLayoutManager(getApplicationContext());
         QuestionnaireRecyclerView.setLayoutManager(QuestionnaireLayoutManager);
 
-        QuestionnaireAdapter = new RecyclerViewAdapter(MainActivity.questionnaire.getQuestions());
+        LinkedList<Question> q = MainActivity.questionnaire.getQuestions();
+
+        int[] Types = new int[q.size()];
+
+        for (int i = 0; i < q.size(); i++) {
+            Question qst = q.get(i);
+            Answer a = qst.getAnswer();
+            switch(a.getType()) {
+                case "Tekst":
+                    Types[i] = RecyclerViewAdapter.Tekst;
+                    break;
+                case "Multiplechoice":
+                    Types[i] = RecyclerViewAdapter.Multiplechoice;
+                    break;
+                case "SingleChoice":
+                    Types[i] = RecyclerViewAdapter.Singlechoice;
+                    break;
+                case "BipolarQuestion":
+                    Types[i] = RecyclerViewAdapter.Bipolarquestion;
+                    break;
+                case "Dichotomous":
+                    Types[i] = RecyclerViewAdapter.Dichotomous;
+                    break;
+                case "GuttmanScale":
+                    Types[i] = RecyclerViewAdapter.Guttmanscale;
+                    break;
+                case "LikertScale":
+                    Types[i] = RecyclerViewAdapter.Likertscale;
+                    break;
+                case "ContinuousScale":
+                    Types[i] = RecyclerViewAdapter.Continuousscale;
+                    break;
+            }
+        }
+
+        QuestionnaireAdapter = new RecyclerViewAdapter(MainActivity.questionnaire.getQuestions(), Types);
         QuestionnaireRecyclerView.setAdapter(QuestionnaireAdapter);
     }
 }
--- app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/RecyclerViewAdapter.java
+++ app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/RecyclerViewAdapter.java
@@ -5,8 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
+import java.sql.Types;
 import java.util.LinkedList;
 
 /**
@@ -15,23 +17,82 @@ import java.util.LinkedList;
  */
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     LinkedList <Question> Questions;
+    int[] TypesQuestions;
 
-    public RecyclerViewAdapter(LinkedList <Question> Questions) {
+    public static final int Tekst = 0;
+    public static final int Multiplechoice = 1;
+    public static final int Singlechoice = 2;
+    public static final int Bipolarquestion = 3;
+    public static final int Guttmanscale = 4;
+    public static final int Likertscale = 5;
+    public static final int Continuousscale = 6;
+    public static final int Dichotomous = 7;
+
+    public RecyclerViewAdapter(LinkedList <Question> Questions, int[] Types) {
         this.Questions = Questions;
+        TypesQuestions = Types;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.dichotomous_card, viewGroup, false);
-        return new ViewHolder(v);
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int Type) {
+        View v;
+        ViewHolder a;
+        if (Type == Dichotomous) {
+            v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.dichotomous_card, viewGroup, false);
+            a = new DichotomousViewHolder(v);
+            return a;
+        } else {
+            v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.dichotomous_card, viewGroup, false);
+            a = new DichotomousViewHolder(v);
+            return a;
+        }
+            /*
+            case Tekst:
+                v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.tekst_card, viewGroup, false);
+                return new ViewHolder(v);
+                break;
+            case Multiplechoise:
+                v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.multiplechoice_card, viewGroup, false);
+                return new ViewHolder(v);
+                break;
+            case Singlechoice:
+                v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.singllechoice_card, viewGroup, false);
+                return new ViewHolder(v);
+                break;
+            case Bipolarquestion:
+                v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.bipolarquestion_card, viewGroup, false);
+                return new ViewHolder(v);
+            break;
+            case Guttmanscale:
+                v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.guttmansacle_card, viewGroup, false);
+                return new ViewHolder(v);
+            break;
+            case Likertscale:
+                v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.likertscale_card, viewGroup, false);
+                return new ViewHolder(v);
+            break;
+            case Continuousscale:
+                v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.continuousscale_card, viewGroup, false);
+                return new ViewHolder(v);
+            break;
+            */
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int i) {
-        Log.i("SS", "int " + i);
-        Log.i("SS", "int " + Questions.get(i).getDescription());
-        Question question = Questions.get(i);
-        viewHolder.description.setText(question.getDescription());
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        if (viewHolder.getItemViewType() == Dichotomous) {
+            Question qst = Questions.get(position);
+            Answer A = qst.getAnswer();
+            LinkedList <AnswerItem> AI = A.getItems();
+            DichotomousViewHolder holder = (DichotomousViewHolder) viewHolder;
+            holder.DichotomousQuestion.setText(qst.getDescription());
+            if (AI.size() > 0) {
+                AnswerItem Item = AI.get(1);
+                holder.DichotomousAnswer1.setText(Item.getItemText());
+                Item = AI.get(2);
+                holder.DichotomousAnswer2.setText(Item.getItemText());
+            }
+        }
     }
 
     @Override
@@ -39,13 +100,26 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return Questions.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public int getItemViewType(int position) {
+        return TypesQuestions[position];
+    }
 
-        private TextView description;
-
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(View itemView) {
             super(itemView);
-            description = (TextView) itemView.findViewById(R.id.DichotomousQuestion);
+        }
+    }
+
+    public class DichotomousViewHolder extends ViewHolder {
+        TextView DichotomousQuestion;
+        RadioButton DichotomousAnswer1, DichotomousAnswer2;
+
+        public DichotomousViewHolder(View v) {
+            super(v);
+            this.DichotomousQuestion = (TextView) v.findViewById(R.id.DichotomousQuestion);
+            this.DichotomousAnswer1 = (RadioButton) v.findViewById(R.id.DichotomousAnswer1);
+            this.DichotomousAnswer2 = (RadioButton) v.findViewById(R.id.DichotomousAnswer2);
         }
     }
 
