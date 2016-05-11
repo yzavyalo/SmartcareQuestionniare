1ff0aabc833a00cc4162a3510d1075c0c6510163
yzavyalo <yzavyalo@cs.karelia.ru>
2016-05-11 10:35:33 +0300

    fixed error in QuestionnaireActivity

--- app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/QuestionnaireActivity.java
+++ app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/QuestionnaireActivity.java
@@ -1,20 +1,10 @@
 package com.petrsu.cardiacare.smartcarequestionnaire;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.view.LayoutInflater;
-
-import java.util.LinkedList;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class QuestionnaireActivity extends AppCompatActivity {
 
@@ -29,11 +19,11 @@ public class QuestionnaireActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        QuestionnaireRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        QuestionnaireRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        QuestionnaireRecyclerView.setHasFixedSize(true);
+        //QuestionnaireRecyclerView.setHasFixedSize(true);
 
-        QuestionnaireLayoutManager= new LinearLayoutManager(this);
+        QuestionnaireLayoutManager= new LinearLayoutManager(getApplicationContext());
         QuestionnaireRecyclerView.setLayoutManager(QuestionnaireLayoutManager);
 
         QuestionnaireAdapter = new RecyclerViewAdapter(MainActivity.questionnaire.getQuestions());
