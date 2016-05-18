6ac0ea84e66a419c431b443dcb3ee3f2c4cc6938
Bassilij <fok-wassya@yandex.ru>
2016-05-19 00:21:43 +0300

    Save Text Result of TextCard
    at exit of QuestionnaireActivity

--- app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/QuestionnaireActivity.java
+++ app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/QuestionnaireActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -44,10 +45,6 @@ public class QuestionnaireActivity extends AppCompatActivity {
         for (int i = 0; i < q.size(); i++) {
             Question qst = q.get(i);
             Answer a = qst.getAnswer();
-            // uri respose == iri question
-            Response resp = new Response(q.get(i).getUri(), q.get(i).getUri());
-            // add to list response
-            MainActivity.feedback.addResponse(resp);
             switch(a.getType()) {
                 case "Text":
                     Types[i] = RecyclerViewAdapter.Tekst;
@@ -87,6 +84,59 @@ public class QuestionnaireActivity extends AppCompatActivity {
     public void onStop() {
         super.onStop();
     //save result
+        LinkedList<Question> q = MainActivity.questionnaire.getQuestions();
+
+        for (int i = 0; i < QuestionnaireRecyclerView.getChildCount(); i++) {
+            //getChildAt(i); return View
+            Question qst = q.get(i);
+            Answer a = qst.getAnswer();
+            // uri respose == iri question
+            Response resp = new Response(q.get(i).getUri(), q.get(i).getUri());
+            // add to list response
+            //MainActivity.feedback.addResponse(resp);
+
+            LinkedList<ResponseItem> AnswerofUser;
+            switch(a.getType()) {
+                case "Text":
+                    ResponseItem TextAnswer = new ResponseItem(a.getUri(), "textItem", "fileUri");
+
+                    AnswerItem f1 = new AnswerItem(a.getItems().get(0));
+                    AnswerItem AnswerText = new AnswerItem(f1.getUri(), f1.getItemScore(), ((EditText)QuestionnaireRecyclerView.getChildAt(i).findViewById(R.id.editText)).getText().toString());
+                    /*
+                    if(((EditText)QuestionnaireRecyclerView.getChildAt(i).findViewById(R.id.editText)).getText().toString() != "") { // (EditText) -- TekstAnswer.getText()
+                        AnswerItem f1 = new AnswerItem(a.getItems().get(0));
+                        AnswerText = new AnswerItem(f1.getUri(), f1.getItemScore(), ((EditText)QuestionnaireRecyclerView.getChildAt(i).findViewById(R.id.editText)).getText().toString());//(a.getItems().get(0));
+                    }
+                    */
+
+                    TextAnswer.addLinkedAnswerItem(AnswerText);
+                    resp.addResponseItem(TextAnswer);
+                    MainActivity.feedback.addResponse(resp);
+                    break;
+                case "MultipleChoise":
+
+                    break;
+                case "SingleChoise":
+
+                    break;
+                case "BipolarQuestion":
+
+                    break;
+                case "Dichotomous":
+
+                    break;
+                case "GuttmanScale":
+
+                    break;
+                case "LikertScale":
+
+                    break;
+                case "ContinuousScale":
+
+                    break;
+                default:
+            }
+        }
     }
 }
 
