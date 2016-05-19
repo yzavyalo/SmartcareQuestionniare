85c450d9ae23123cc0dfa46fe846994d641c73a6
Bassilij <fok-wassya@yandex.ru>
2016-05-19 18:13:36 +0300

    Save MultipleChoise Result of MultipleChoiseCard

--- app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/QuestionnaireActivity.java
+++ app/src/main/java/com/petrsu/cardiacare/smartcarequestionnaire/QuestionnaireActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -89,32 +90,52 @@ public class QuestionnaireActivity extends AppCompatActivity {
         for (int i = 0; i < QuestionnaireRecyclerView.getChildCount(); i++) {
             //getChildAt(i); return View
             Question qst = q.get(i);
-            Answer a = qst.getAnswer();
+            Answer ans = qst.getAnswer();
             // uri respose == iri question
             Response resp = new Response(q.get(i).getUri(), q.get(i).getUri());
             // add to list response
             //MainActivity.feedback.addResponse(resp);
 
             LinkedList<ResponseItem> AnswerofUser;
-            switch(a.getType()) {
+            switch(ans.getType()) {
                 case "Text":
-                    ResponseItem TextAnswer = new ResponseItem(a.getUri(), "textItem", "fileUri");
-
-                    AnswerItem f1 = new AnswerItem(a.getItems().get(0));
-                    AnswerItem AnswerText = new AnswerItem(f1.getUri(), f1.getItemScore(), ((EditText)QuestionnaireRecyclerView.getChildAt(i).findViewById(R.id.editText)).getText().toString());
+                    // ответ, с ИД вответа и какими-то параметрами, разберусь потом
+                    ResponseItem TextAnswer = new ResponseItem(ans.getUri(), "textItem", "fileUri");
+                    // получаю 0 элемент из списка Answer, TextCard только один ответ
+                    AnswerItem ansitforText = new AnswerItem(ans.getItems().get(0));
+                    // получаю значение из EditText, в других это заранее известно (только надо выбрать отмеченный)
+                    AnswerItem AnswerText = new AnswerItem(ansitforText.getUri(), ansitforText.getItemScore(), ((EditText)QuestionnaireRecyclerView.getChildAt(i).findViewById(R.id.editText)).getText().toString());
                     /*
                     if(((EditText)QuestionnaireRecyclerView.getChildAt(i).findViewById(R.id.editText)).getText().toString() != "") { // (EditText) -- TekstAnswer.getText()
                         AnswerItem f1 = new AnswerItem(a.getItems().get(0));
                         AnswerText = new AnswerItem(f1.getUri(), f1.getItemScore(), ((EditText)QuestionnaireRecyclerView.getChildAt(i).findViewById(R.id.editText)).getText().toString());//(a.getItems().get(0));
                     }
                     */
-
+                    // Собираю полученное значение в ответ
                     TextAnswer.addLinkedAnswerItem(AnswerText);
+                    // Собираю полученное значение в "вопрос"
                     resp.addResponseItem(TextAnswer);
+                    // Собираю и отправляю в главную программу, вопрос с ответом (на который ответил пользователь)
                     MainActivity.feedback.addResponse(resp);
                     break;
                 case "MultipleChoise":
-
+                    // ответ, с ИД вответа и какими-то параметрами, разберусь потом
+                    ResponseItem MultipleChoiseAnswer = new ResponseItem(ans.getUri(), "textItem", "fileUri");
+                    for(int j = 0; j < ans.getItems().size(); j++) {
+                        // получаю j элемент из списка Answer, MultipleChoiseCard много ответов
+                        AnswerItem ansitforMultipleChoise = new AnswerItem(ans.getItems().get(j));
+                        // если из списка LinearLayout есть выбранный CheckBox
+                        if(( (CheckBox) ((LinearLayout)QuestionnaireRecyclerView.getChildAt(i).findViewById(R.id.LinearMultiple)).getChildAt(j)).isChecked()) {
+                            // получаю значение из CheckBox, точнее из списка ответов к данному вопросу
+                            AnswerItem AnswerMultipleChoise = new AnswerItem(ansitforMultipleChoise.getUri(), ansitforMultipleChoise.getItemScore(), ansitforMultipleChoise.getItemText()/**/);
+                            // Собираю полученное значение в ответ
+                            MultipleChoiseAnswer.addLinkedAnswerItem(AnswerMultipleChoise);
+                            // Собираю полученное значение в "вопрос"
+                            resp.addResponseItem(MultipleChoiseAnswer);
+                        }
+                    }
+                    // Собираю и отправляю в главную программу, вопрос с ответом (на который ответил пользователь)
+                    MainActivity.feedback.addResponse(resp);
                     break;
                 case "SingleChoise":
 
